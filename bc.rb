2e8c97d1aedd2f311bf01dda0b1096c35ecfdc08
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-13 20:01:02 +0100

    bc: add 1.06 bottle.

--- bc.rb
+++ bc.rb
@@ -5,6 +5,13 @@ class Bc < Formula
   mirror "https://ftp.gnu.org/gnu/bc/bc-1.06.tar.gz"
   sha256 "4ef6d9f17c3c0d92d8798e35666175ecd3d8efac4009d6457b5c99cea72c0e33"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "def84a9cd7f95263f20ebc17e3a47d529c9a8568315cc2754c6072b281259d28" => :el_capitan
+    sha256 "d3d8fa4f7eb53ac19b43f576ffb8f58a985de1a2adf689e439a78899abefac3f" => :yosemite
+    sha256 "7438af8ab383f113f0689d957961ead1e9c51f9e191287392c95bdbe35cba857" => :mavericks
+  end
+
   keg_only :provided_by_osx
 
   def install
