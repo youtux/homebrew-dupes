65c40f6f51564e06f5575cdebf0e313b7b576ebb
Frank <FrankSansC@users.noreply.github.com>
2015-12-24 15:31:57 +0100

    nano 2.5.0
    Closes #539.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- nano.rb
+++ nano.rb
@@ -1,16 +1,8 @@
 class Nano < Formula
   desc "A free (GNU) replacement for the Pico text editor"
   homepage "http://www.nano-editor.org/"
-  url "http://www.nano-editor.org/dist/v2.4/nano-2.4.2.tar.gz"
-  sha256 "c8cd7f18fcf5696d9df3364ee2a840e0ab7b6bdbd22abf850bbdc951db7f65b9"
-  revision 1
-
-  bottle do
-    cellar :any
-    sha256 "f282672a102b5b9f1034c3ee46792394b341dce14856dc6746cb56ae15eda6f2" => :yosemite
-    sha256 "9803757b95a170d6bcc1ca7a60a78b2e5c964d0d5af17e700b8aafcf3d9a9528" => :mavericks
-    sha256 "25623a18d91f5325386e2e25cab2137e54a5d02b71657931024c731916dcf58a" => :mountain_lion
-  end
+  url "http://www.nano-editor.org/dist/v2.5/nano-2.5.0.tar.gz"
+  sha256 "ff323e6fef74caf0a924304841c07ac65ec30db99dc5c1f8272b4c536a5c89ee"
 
   head do
     url "svn://svn.sv.gnu.org/nano/trunk/nano"
