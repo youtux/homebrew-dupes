f107ef880c852ef162232f804c1330e961877b41
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-02 22:52:26 +0000

    nano: add 2.5.0 bottle.

--- nano.rb
+++ nano.rb
@@ -4,6 +4,13 @@ class Nano < Formula
   url "http://www.nano-editor.org/dist/v2.5/nano-2.5.0.tar.gz"
   sha256 "ff323e6fef74caf0a924304841c07ac65ec30db99dc5c1f8272b4c536a5c89ee"
 
+  bottle do
+    cellar :any
+    sha256 "35b4a4014ab36ccfa89aa38c74ae8179c26de5d786d08fee33a852ab397ad0b9" => :el_capitan
+    sha256 "f42d3c2164c916f2ae0596bd30b64251568b710e79b7260e80a655abbef66f51" => :yosemite
+    sha256 "e21a36b319b7a8abdc5f3a082f681d773ad1c8337b94805636c542594ae07798" => :mavericks
+  end
+
   head do
     url "svn://svn.sv.gnu.org/nano/trunk/nano"
     depends_on "automake" => :build
