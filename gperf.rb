8705b9f3de5fcb23b27787b7981b580fc10c22b0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-10 02:05:16 +0000

    gperf: add 3.0.4 bottle.

--- gperf.rb
+++ gperf.rb
@@ -5,6 +5,13 @@ class Gperf < Formula
   mirror "https://ftp.gnu.org/pub/gnu/gperf/gperf-3.0.4.tar.gz"
   sha256 "767112a204407e62dbc3106647cf839ed544f3cf5d0f0523aaa2508623aad63e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7a29550946fb75b6ec7796c00563038727843edf975c5a1f001e9a2334ea2741" => :el_capitan
+    sha256 "b59111d8699e9e005fd8ba71f1c73082801c59618f69e3b6132ef17f4fc3d4d4" => :yosemite
+    sha256 "2c96a9dac35ab5cc085d778d0e1fa4899dd0ae2de3120f243a9513761c3eda02" => :mavericks
+  end
+
   keg_only :provided_until_xcode43
 
   def install
