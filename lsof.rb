8f784247f2b64cbc491232f66c8f1f764a3d0e3e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-10 13:55:40 +0000

    lsof: add 4.88 bottle.

--- lsof.rb
+++ lsof.rb
@@ -5,6 +5,13 @@ class Lsof < Formula
   mirror "http://mirror.jaredwhiting.net/distfiles/lsof_4.88.tar.bz2"
   sha256 "fe6f9b0e26b779ccd0ea5a0b6327c2b5c38d207a6db16f61ac01bd6c44e5c99b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "166741406a0a1d6ac78b82274d56f1bb1d24c4d4cd2421919e48ce42566f90ca" => :el_capitan
+    sha256 "b8292349936eea9e0f24daf01818cba06ce5e689b4e97f026a431034c509e67d" => :yosemite
+    sha256 "92a1f687aa8a0df343b90771e16f6a11aa60968c034c9660318e38142533fb82" => :mavericks
+  end
+
   def install
     ENV["LSOF_INCLUDE"] = "#{MacOS.sdk_path}/usr/include"
 
