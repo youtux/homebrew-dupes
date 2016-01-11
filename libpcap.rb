780d69ce5a0ec986f59e4bc666ff77dfa18ea692
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-11 11:59:35 +0100

    libpcap: audit fix
    Closes #560.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- libpcap.rb
+++ libpcap.rb
@@ -3,6 +3,7 @@ class Libpcap < Formula
   homepage "http://www.tcpdump.org/"
   url "http://www.tcpdump.org/release/libpcap-1.7.4.tar.gz"
   sha256 "7ad3112187e88328b85e46dce7a9b949632af18ee74d97ffc3f2b41fe7f448b0"
+  head "git://bpf.tcpdump.org/libpcap"
 
   bottle do
     cellar :any
@@ -12,8 +13,6 @@ class Libpcap < Formula
   end
 
   keg_only :provided_by_osx
-
-  head "git://bpf.tcpdump.org/libpcap"
 
   def install
     system "./configure", "--prefix=#{prefix}",
