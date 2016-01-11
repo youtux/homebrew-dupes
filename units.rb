8a05b9e83acd5ac12606f880ad62c6b7b8ab5d3c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-11 11:25:14 +0000

    units: add 2.01 bottle.

--- units.rb
+++ units.rb
@@ -5,6 +5,12 @@ class Units < Formula
   mirror "https://ftp.gnu.org/gnu/units/units-2.01.tar.gz"
   sha256 "138b12c70bffa0e484fdfe579927412b1ac573e8351c17d8938592f9ba9a88f2"
 
+  bottle do
+    sha256 "895f1cd13ff45a9dd1756a44a4159b550480ddff895a13f9336b316752d0d7ac" => :el_capitan
+    sha256 "643f93a9a83081a28932b8ffad744db0bdf61723ada1bb7686f9dee2af26aa41" => :yosemite
+    sha256 "3897bdd0c6a5ca372f48d39b598df768385ac059c171f5e2df7f9a52bc056839" => :mavericks
+  end
+
   keg_only :provided_by_osx,
     "OS X provides BSD units, which behaves differently from GNU units."
 
