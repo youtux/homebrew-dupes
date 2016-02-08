5df8b5560eed14096e3ce2bd3865a7c297d5fcdb
Maxim Belkin <maxim.belkin@gmail.com>
2016-02-08 10:07:21 -0600

    tcl-tk: don't apply patch for sqlite on Linux machines
    Closes #566.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- tcl-tk.rb
+++ tcl-tk.rb
@@ -37,7 +37,7 @@ class TclTk < Formula
 
   # sqlite won't compile on Tiger due to missing function;
   # patch submitted upstream: http://thread.gmane.org/gmane.comp.db.sqlite.general/83257
-  patch :DATA if MacOS.version < :leopard
+  patch :DATA if (OS.mac? && MacOS.version < :leopard)
 
   def install
     args = ["--prefix=#{prefix}", "--mandir=#{man}"]
