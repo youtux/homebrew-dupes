71573ecb29a19986670e997df01660ee19bd2143
Bob W. Hogg <rwhogg@linux.com>
2016-03-19 21:40:41 -0700

    openssh: audit fixes
    Closes #582.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- openssh.rb
+++ openssh.rb
@@ -29,8 +29,8 @@ class Openssh < Formula
     end
 
     patch do
-     url "https://raw.githubusercontent.com/Homebrew/patches/d8b2d8c2/openssh/patch-sshd.c-apple-sandbox-named-external.diff"
-     sha256 "3505c58bf1e584c8af92d916fe5f3f1899a6b15cc64a00ddece1dc0874b2f78f"
+      url "https://raw.githubusercontent.com/Homebrew/patches/d8b2d8c2/openssh/patch-sshd.c-apple-sandbox-named-external.diff"
+      sha256 "3505c58bf1e584c8af92d916fe5f3f1899a6b15cc64a00ddece1dc0874b2f78f"
     end
 
     # Patch for SSH tunnelling issues caused by launchd changes on Yosemite
