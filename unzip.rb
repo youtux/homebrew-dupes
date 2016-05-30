faeb12a2f67d9edd82bdebad11f8383c5f217594
Felix Bünemann <buenemann@louis.info>
2016-05-30 21:53:43 +0200

    unzip: Add Ubuntu patchset, enable ZIP64 support
    * Use Ubuntu patchset for unzip, with lots of bug and security fixes.
    * Enable large file support / ZIP64 for >=4GB and >=64k files.
    * Compile against latest system libbz2 instead of built-in.
    
    Closes #613.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- unzip.rb
+++ unzip.rb
@@ -4,11 +4,45 @@ class Unzip < Formula
   url "https://downloads.sourceforge.net/project/infozip/UnZip%206.x%20%28latest%29/UnZip%206.0/unzip60.tar.gz"
   version "6.0"
   sha256 "036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37"
+  revision 1
 
   keg_only :provided_by_osx
 
+  # Upstream is unmaintained so we use the Ubuntu unzip-6.0-20ubuntu1 patchset:
+  # http://changelogs.ubuntu.com/changelogs/pool/main/u/unzip/unzip_6.0-20ubuntu1/changelog
+  patch do
+    url "https://launchpad.net/ubuntu/+archive/primary/+files/unzip_6.0-20ubuntu1.debian.tar.xz"
+    mirror "http://archive.ubuntu.com/ubuntu/pool/main/u/unzip/unzip_6.0-20ubuntu1.debian.tar.xz"
+    sha256 "0ddf122ef15b739e3ea06db4b9e80f40759dce23a2c886678881453a43bd0842"
+    apply %w[
+      patches/01-manpages-in-section-1-not-in-section-1l
+      patches/02-branding-patch-this-is-debian-unzip
+      patches/03-include-unistd-for-kfreebsd
+      patches/04-handle-pkware-verification-bit
+      patches/05-fix-uid-gid-handling
+      patches/06-initialize-the-symlink-flag
+      patches/07-increase-size-of-cfactorstr
+      patches/08-allow-greater-hostver-values
+      patches/09-cve-2014-8139-crc-overflow
+      patches/10-cve-2014-8140-test-compr-eb
+      patches/11-cve-2014-8141-getzip64data
+      patches/12-cve-2014-9636-test-compr-eb
+      patches/13-remove-build-date
+      patches/14-cve-2015-7696
+      patches/15-cve-2015-7697
+      patches/16-fix-integer-underflow-csiz-decrypted
+      patches/20-unzip60-alt-iconv-utf8
+    ]
+  end
+
   def install
-    system "make", "-f", "unix/Makefile", "macosx"
+    system "make", "-f", "unix/Makefile",
+      "CC=#{ENV.cc}",
+      "LOC=-DLARGE_FILE_SUPPORT",
+      "D_USE_BZ2=-DUSE_BZIP2",
+      "L_BZ2=-lbz2",
+      "LFLAGS1=-liconv",
+      "macosx"
     system "make", "prefix=#{prefix}", "MANDIR=#{man}", "install"
   end
 
