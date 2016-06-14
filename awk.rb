d49f9f90d58ee8f47ae51f1c66dd3c85c956f06d
Dominyk Tiller <dominyktiller@gmail.com>
2016-06-14 19:57:26 +0100

    Revert "update awk formula"
    This reverts commit 4dfe8686d97b8942d197d4ff4b5957471ea2aaff.
    
    I'm not happy with this as-is. In the original discussion we advised OP to host
    this formula in their own tap, but we've ended up with it replacing the dupes
    formula with minimal discussion.
    
    Stylistically the formula is all over the shop. It hasn't been through CI in
    ~6 months or more so it cannot possibly be known if it'll actually build today, or passes
    audit/etc. The secure URLs were downgraded without any justification. There's merit
    to discussing whether we even qualify `head` as a valid upstream source we're happy
    with.
    
    I should have closed this at the time it was submitted with similar comments, but
    I let it sit, so I accept that's my fault for not being clear that I wasn't
    thrilled as-is.

--- awk.rb
+++ awk.rb
@@ -1,58 +1,21 @@
 class Awk < Formula
-  desc "Brian Kernighan's pattern scanning and processing language"
-  homepage "http://www.cs.princeton.edu/~bwk/btl.mirror/"
-  url "http://www.cs.princeton.edu/~bwk/btl.mirror/awk.tar.gz"
+  desc "Text processing scripting language"
+  homepage "https://www.cs.princeton.edu/~bwk/btl.mirror/"
+  url "https://www.cs.princeton.edu/~bwk/btl.mirror/awk.tar.gz"
   version "20121220"
   sha256 "8dc092165c5a4e1449f964286483d06d0dbfba4b0bd003cb5dab30de8f6d9b83"
-  head "https://github.com/junghans/nawk.git", :branch => "gentoo"
+
+  conflicts_with "gawk",
+    :because => "both install awk executables."
 
   def install
-    # have to run bison first or parallel build fails
-    system "make", "YACC=bison -d -y", "ytab.o"
-    system "make"
-    bin.install "a.out" => "nawk"
-
-    (libexec/"xpg4bin").install_symlink bin/"nawk" => "awk"
-
-    cp "awk.1", "awk.1.bak"
-
-    # install original manpage to xpg4man before rewriting to "nawk"
-    (libexec/"xpg4man"/"man1").install "awk.1"
-
-    mv "awk.1.bak", "awk.1"
-
-    # refer to 'nawk' instead of 'awk' in man page, and add a couple of
-    # SEE ALSO items
-    [
-      ["awk", "nawk"], ["AWK", "NAWK"], ["Awk", "Nawk"],
-      [/^.IR sed \(1\)$/, ".IR sed (1),\n.IR gsed (1),\n.IR awk (1),\n.IR gawk (1)"],
-    ].each do |s|
-      inreplace "awk.1", *s
-    end
-
-    man1.install "awk.1" => "nawk.1"
-  end
-
-  def caveats; <<-EOS.undent
-    This is Brian Kernighan's "new awk" or "BWK awk". It has been
-    installed as "nawk", and you can access the man page as "man nawk".
-
-    If you want to use it as "awk" add the "xpg4bin" directory to your
-    PATH from your bashrc like:
-
-        PATH="#{opt_libexec}/xpg4bin:$PATH"
-
-    And you can access the man page by adding the "xpg4man" directory to
-    your MANPATH from your bashrc as follows:
-
-        MANPATH="#{opt_libexec}/xpg4man:$MANPATH"
-
-    EOS
-  end
-
-  test do
-    assert_match(/^Working!$/,
-      shell_output("#{bin}/nawk 'BEGIN { print \"Working!\"; exit(0); }'"),
-                )
+    ENV.O3 # Docs recommend higher optimization
+    # the yacc command the makefile uses results in build failures:
+    # /usr/bin/bison: missing operand after `awkgram.y'
+    # makefile believes -S to use sprintf instead of sprint, but the
+    # -S flag is not supported by `bison -y`
+    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}", "YACC=yacc -d"
+    bin.install "a.out" => "awk"
+    man1.install "awk.1"
   end
 end
